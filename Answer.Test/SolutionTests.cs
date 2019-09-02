16615c20ec1f7a1e79175b5a9bd86fd62f9a192b
neal.nankivell <neal.nankivell@gmail.com>
2019-09-02 09:20:44 +0200

    Added failing tests for some edge cases
    This initial solution missed the edge cases when
    the last number in the collection is removed.

--- Answer.Test/SolutionTests.cs
+++ Answer.Test/SolutionTests.cs
@@ -7,6 +7,10 @@ namespace Tests
     {
         [TestCase(3, 0, 1, ExpectedResult = 2)]
         [TestCase(9, 6, 4, 2, 3, 5, 7, 0, 1, ExpectedResult = 8)]
+        [TestCase(0, ExpectedResult = 1)]
+        [TestCase(0, 1, ExpectedResult = 2)]
+        [TestCase(0, 1, 2, ExpectedResult = 3)]
+        [TestCase(ExpectedResult = 0)]
         public int MissingNumber(params int[] nums)
         {
             var sut = new Solution();
